efa7e7a19e57d261304a197e0cac2f97e9aad56a
Gaurav <gaurav.s@cisinlabs.com>
2024-06-19 16:16:07 +0530

    acciden corrected

--- app/controllers/api/v1/accidents_controller.rb
+++ app/controllers/api/v1/accidents_controller.rb
@@ -14,8 +14,8 @@ module Api
         @accident.company = current_user.company
 
         if !(params["country"] == "United Kingdom" || params["country"] == "England")
-          @video.lat = 53.614345750544985
-          @video.long = -2.1519367845205943
+          @accident.lat = 53.614345750544985
+          @accident.long = -2.1519367845205943
         end
 
         if @accident.save
